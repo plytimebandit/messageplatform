9c55b13523bce071157b47a7a9a05e7e4f474c9a
plytimebandit <spallek.kj@gmail.com>
2017-07-24 21:15:17 +0200

    Refactored code: Injecting examples now

--- src/main/java/org/plytimebandit/messaging/Main.java
+++ src/main/java/org/plytimebandit/messaging/Main.java
@@ -3,7 +3,7 @@ package org.plytimebandit.messaging;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.plytimebandit.messaging.util.Payload;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.messaging.Message;
@@ -14,18 +14,27 @@ public class Main {
 
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
-    public static void main(String... args) {
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
-                "/META-INF/spring/integration.xml");
+    private MessageChannel example01Channel;
 
-        MessageChannel channel = context.getBean("example01Channel", MessageChannel.class);
+    public static void main(String... args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/integration.xml");
+
+        Main main = context.getBean(Main.class);
+        main.runExample01();
+    }
+
+    private void runExample01() {
         Message<Payload> message = MessageBuilder.withPayload(
                 new Payload(3)
         ).build();
 
         MessagingTemplate messagingTemplate = new MessagingTemplate();
-        Message<?> returnMessage = messagingTemplate.sendAndReceive(channel, message);
+        Message<?> returnMessage = messagingTemplate.sendAndReceive(example01Channel, message);
 
         LOG.info("Return message: " + returnMessage.getPayload());
+    }
+
+    public void setExample01Channel(MessageChannel example01Channel) {
+        this.example01Channel = example01Channel;
     }
 }
